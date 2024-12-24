e7187f0fa8f05c58b63ae453c07bcf525a31bda9
ivnlsv <ivan.losev@denuo.legal>
2024-12-24 11:53:02 +0300

    Home work done

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const title = '';
-const screens = '';
-const screenPrice = '';
-const rollback ='';
-const fullPrice ='';
-const adaptive = '';
-console.log('Это консоль');
-alert('Это алерт');
+const title = "Lesson 2";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 200;
+const rollback = 10;
+const fullPrice = 500;
+const adaptive = true;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость  верстки экранов" + " " + screenPrice + " " + "рублей/ долларов/ гривен/ юани");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей/ долларов/ гривен/ юани");
+screenLower = screens.toLowerCase()
+console.log(screenLower.split(", "));
+dealerPercent = (fullPrice * (rollback / 100));
+console.log("Процент отката посреднику:" + " " + dealerPercent)
