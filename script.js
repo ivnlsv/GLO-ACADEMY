1ebb9f632d7b17264d488aad69ff5da0779199bc
ivnlsv <ivan.losev@denuo.legal>
2024-12-24 16:35:22 +0300

    Done

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const title = '';
-const screens = '';
-const screenPrice = '';
-const rollback ='';
-const fullPrice ='';
-const adaptive = '';
-console.log('Это консоль');
-alert('Это алерт');
+let lang = "ru";
+weekDaysRu = "Пн, Вт, Ср, Чт, Пт, Сб, Вс";
+weekDaysEng = "Mon, Tue, Web, Thu, Fri, Sat, Sun";
+
+if (lang === "ru") {
+  console.log(weekDaysRu);
+} else {
+  console.log(weekDaysEng);
+}
+
+switch (lang) { 
+    case "ru":
+        console.log(weekDaysRu);
+        break;
+    case "en":
+        console.log(weekDaysEng);
+        break;
+}
+
+let weekDaysArray = {
+  ru: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+  en: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+};
+
+let days = weekDaysArray[lang] || []; 
+
+console.log(days);
