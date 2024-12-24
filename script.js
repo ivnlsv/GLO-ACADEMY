972db56251cbba3eb54d210bcfda8b35c67f64ee
ivnlsv <ivan.losev@denuo.legal>
2024-12-24 15:40:41 +0300

    Update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 'use strict'
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = prompt("Сколько будет стоить данная работа?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let rollback = 10;
 let adaptive = prompt("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = prompt("Сколько это будет стоить?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
-let servicePrice2 = prompt("Сколько это будет стоить?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let dealerPercent = (fullPrice * (rollback / 100));
 let servicePercentPrice = fullPrice - dealerPercent;
-console.log(Math.ceil(servicePercentPrice))
+console.log(Math.ceil(servicePercentPrice));
