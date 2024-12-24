85387c5a4ced9af6856c6ca92b94f809e2e220a5
ivnlsv <ivan.losev@denuo.legal>
2024-12-24 15:52:17 +0300

    Update

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let dealerPercent = (fullPrice * (rollback / 100));
 let servicePercentPrice = fullPrice - dealerPercent;
 console.log(Math.ceil(servicePercentPrice));
+switch (true) { 
+    case fullPrice > 30000:
+        console.log("Даем скидку в 10%");
+        break;
+    case fullPrice > 15000 && fullPrice < 30000:
+        console.log("Даем скидку в 5%");
+        break;
+    case fullPrice > 0 && fullPrice < 15000:
+            console.log("Скидка не предусмотрена");
+        break;
+    case fullPrice < 0:
+            console.log("Что то пошло не так");
+        break;
+}
