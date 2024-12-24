dfa9d0dd250d672c39189f705bf07ccead75a9d9
ivnlsv <ivan.losev@denuo.legal>
2024-12-24 15:23:40 +0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const title = "Lesson 2";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 200;
-const rollback = 10;
-const fullPrice = 500;
-const adaptive = true;
+'use strict'
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = prompt("Сколько будет стоить данная работа?");
+let rollback = 10;
+let adaptive = prompt("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+let servicePrice2 = prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+dealerPercent = (fullPrice * (rollback / 100));
+servicePercentPrice = fullPrice - dealerPercent;
+console.log(servicePercentPrice)
