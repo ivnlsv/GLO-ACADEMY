6e5454619f9e70ae1e69db703e3de86621f6e86e
ivnlsv <ivan.losev@denuo.legal>
2024-12-25 13:10:07 +0300

    Updated

--- script.js
+++ script.js
@@ -8,21 +8,34 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let dealerPercent = (fullPrice * (rollback / 100));
-let servicePercentPrice = fullPrice - dealerPercent;
-console.log(Math.ceil(servicePercentPrice));
-switch (true) { 
-    case fullPrice >= 30000:
-        console.log("Даем скидку в 10%");
-        break;
-    case fullPrice >= 15000 && fullPrice < 30000:
-        console.log("Даем скидку в 5%");
-        break;
-    case fullPrice > 0 && fullPrice < 15000:
-            console.log("Скидка не предусмотрена");
-        break;
-    case fullPrice <= 0:
-            console.log("Что то пошло не так");
-        break;
+//let fullPriceMain = screenPrice + servicePrice1 + servicePrice2;
+//let dealerPercent = (fullPriceMain * (rollback / 100));
+//let servicePercentPrice = fullPriceMain - dealerPercent;
+//console.log(Math.ceil(servicePercentPrice));
+/* switch (true) {  */
+/*     case fullPriceMain >= 30000: */
+/*         console.log("Даем скидку в 10%"); */
+/*         break; */
+/*     case fullPriceMain >= 15000 && fullPriceMain < 30000: */
+/*         console.log("Даем скидку в 5%"); */
+/*         break; */
+/*     case fullPriceMain > 0 && fullPriceMain < 15000: */
+/*             console.log("Скидка не предусмотрена"); */
+/*         break; */
+/*     case fullPriceMain <= 0: */
+/*             console.log("Что то пошло не так"); */
+/*         break; */
+/* } */
+const getAllServicePrices = function (servicePrice1, servicePrice2) { 
+  return servicePrice1 + servicePrice2;
 }
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+function getFullPrice(screenPrice, allServicePrices) { 
+  return screenPrice + allServicePrices;
+} 
+
+let fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+console.log("Стоимость доп. услуг:" + " " + allServicePrices);
+console.log("Полная стоимость:" +  " " + fullPrice);
