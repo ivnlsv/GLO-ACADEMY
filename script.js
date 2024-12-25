e1d94d216c56456adee2a61f92bdd1fb768d2662
ivnlsv <ivan.losev@denuo.legal>
2024-12-25 15:13:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
@@ -9,20 +9,15 @@ let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let dealerPercent = (fullPrice * (rollback / 100));
-let servicePercentPrice = fullPrice - dealerPercent;
-console.log(Math.ceil(servicePercentPrice));
-switch (true) { 
-    case fullPrice >= 30000:
-        console.log("Даем скидку в 10%");
-        break;
-    case fullPrice >= 15000 && fullPrice < 30000:
-        console.log("Даем скидку в 5%");
-        break;
-    case fullPrice > 0 && fullPrice < 15000:
-            console.log("Скидка не предусмотрена");
-        break;
-    case fullPrice <= 0:
-            console.log("Что то пошло не так");
-        break;
+let dealerPercent = fullPrice * (rollback / 100);
+let servicePercentPrice = Math.ceil(fullPrice - dealerPercent);
+console.log(servicePercentPrice);
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice > 0 && fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+  console.log("Что то пошло не так");
 }
