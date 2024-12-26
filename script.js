73b10b60de9b03b186818f12247efe32495c1b11
ivnlsv <ivan.losev@denuo.legal>
2024-12-26 14:15:12 +0300

    Добавлены изменения в соответствии с правилами написания кода

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let rollback = 10;
 let adaptive = prompt("Нужен ли адаптив на сайте?");
+
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let ServicePercentPrices;
+
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
@@ -16,13 +21,10 @@ const showTypeOf = function (variable) {
 const getAllServicePrices = function (servicePrice1, servicePrice2) {
   return servicePrice1 + servicePrice2;
 };
-let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
 function getFullPrice(screenPrice, allServicePrices) {
   return screenPrice + allServicePrices;
 }
-
-let fullPrice = getFullPrice(screenPrice, allServicePrices);
 
 const getRollbackMessage = function (price) {
   if (price >= 30000) {
@@ -49,13 +51,15 @@ function getServicePercentPrices(rollBack, totalPrice) {
   return totalPrice - dealerPercent;
 }
 
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+ServicePercentPrices = getServicePercentPrices();
+title = getTitle();
+
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
+
 console.log(screens);
 console.log(getRollbackMessage(fullPrice));
-console.log(
-  "Стоимость за вычетом отката: " +
-    " " +
-    getServicePercentPrices(rollback, fullPrice)
-);
+console.log("Стоимость за вычетом отката: " +  " " + ServicePercentPrices);
