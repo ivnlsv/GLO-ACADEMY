268d0e0934633572d702399c75f72bd5d9c0ed91
ivnlsv <ivan.v.losev@gmail.com>
2025-01-06 17:25:51 +0300

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 let userInput = +prompt("Угадай число от 1 до 100");
 let randomNum = Math.floor(Math.random() * 100) + 1;
+console.log(randomNum);
 if (userInput > randomNum) {
   alert("Загаданное число меньше");
 } else if (userInput < randomNum) {
